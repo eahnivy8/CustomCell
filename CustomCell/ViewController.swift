2f8031de8972e7d074fd13029d5856fced0988a4
Eddie <eahnivy8@gmail.com>
2019-09-17 12:58:20 +0900

    Initial Commit

---
+++ CustomCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomCell
+//
+//  Created by Eddie Ahn on 17/09/2019.
+//  Copyright © 2019 Eddie Ahn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
